dff66f7d3d2812a1467e985d45e7a87a9d31b6ae
Alec Xiang <alec.xiang@yale.edu>
2021-12-07 23:51:01 -0500

    10 problems done

--- 1to10.hs
+++ 1to10.hs
@@ -1,3 +1,5 @@
+-- Problem source: https://wiki.haskell.org/99_questions/1_to_10
+
 -- Problem 1
 myLast :: [a] -> a
 myLast [] = error "no last element"
@@ -17,3 +19,39 @@ elementAt x y = x !! (y-1)
 myLength :: [a] -> Int
 myLength [] = 0
 myLength (_:x) = 1 + myLength x
+
+-- Problem 5
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+-- Problem 6
+isPalindrome :: Eq a => [a] -> Bool
+isPalindrome [] = True 
+isPalindrome [x] = True
+isPalindrome xs = (head xs == last xs) && isPalindrome (init (tail xs))
+                    
+-- Problem 7
+data NestedList a = Elem a | List [NestedList a]
+flatten :: NestedList a -> [a]
+flatten (Elem x) = [x]
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+flatten (List []) = []
+
+-- Problem 8
+compress :: Eq a => [a] -> [a]
+compress [] = []
+compress [x] = [x]
+compress (x:xs) = if x == head xs
+                    then compress xs
+                    else x:compress xs
+
+-- Problem 9
+pack :: Eq a => [a] -> [[a]]
+pack (x:xs) = let (first, rest) = span (==x) xs
+                in (x:first) : pack rest
+pack [] = []
+
+-- Problem 10
+encode :: Eq a => [a] -> [(Int, a)]
+encode xs = [(length x, head x) | x <- pack xs]
